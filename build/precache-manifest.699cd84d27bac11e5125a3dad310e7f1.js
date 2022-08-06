95cd84b3bc2be79f07012fc69c3178b8f2537921
Victor Agbeo <75548415+victoragbeo@users.noreply.github.com>
2022-08-06 15:07:41 -0500

    n

---
+++ build/precache-manifest.699cd84d27bac11e5125a3dad310e7f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45f3a5eae33ed11c72dac4cc89cd71f",
+    "url": "/note-keeper/index.html"
+  },
+  {
+    "revision": "e0bfa52abe8f52825d2f",
+    "url": "/note-keeper/static/js/2.67111b69.chunk.js"
+  },
+  {
+    "revision": "8b85938de9b3951a56ae",
+    "url": "/note-keeper/static/js/main.fc297f8a.chunk.js"
+  },
+  {
+    "revision": "466b86872f796ca5f2bb",
+    "url": "/note-keeper/static/js/runtime-main.a82c5170.js"
+  }
+]);
